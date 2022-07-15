f5473583cd46a10360d0fb3ba5a595ab77a9c784
Phillip Erasmus <erasmuspb87@gmail.com>
2022-07-15 13:18:36 +0200

    recommit also host info for knex / heroku fixed

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors');
+const knex = require('knex');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : process.env.DATABASE_URL,
+      ssl: true
+    }
+});
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('success')
+});
+
+app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App is running on port 3000');
+});
